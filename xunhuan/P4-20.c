0bd78caac86987dbc8183f0b589df1ac0bb88fac
Your Name <you@example.com>
2020-02-11 23:03:20 +0800

    2-11下午作业

--- xunhuan/P4-20.c
+++ xunhuan/P4-20.c
@@ -2,5 +2,20 @@
 
 #include <stdio.h>
 int main(){
-    
+    printf("       ");
+    for(int a=1;a<10;a++){
+        printf("%4d ",a);
+    }
+    putchar('\n');
+    for(int i=1;i<=55;i++){
+        putchar('-');
+    }
+    putchar('\n');
+    for(int i=1;i<=9;i++){
+        printf("%4d  |",i);
+        for(int j=1;j<=9;j++){
+            printf("%4d ",i*j);
+        }
+        putchar('\n');
+    }
 }
